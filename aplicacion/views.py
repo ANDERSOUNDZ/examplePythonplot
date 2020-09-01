032665a7ec72d207f1e8f13008192567c82a1fc1
Anderson Chanchay <andersonchanchay@iMac-de-Anderson.local>
2020-08-31 21:54:59 -0500

    mija chulña

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,15 +1,6 @@
 
 from django.shortcuts import render
-# import matplotlib.pyplot as plt
-# import base64
-# from io import BytesIO
 
-
-# def home(request):
-    
-#     return render(request,'home.html')
-
-#matplotlib.rcParams['text.usetex'] = True
 import matplotlib
 import matplotlib.pyplot as plt
 import io
@@ -17,36 +8,29 @@ import urllib, base64
 import numpy as np
 
 
-
 # Create your views here.
 
-
-
 # def home(request):
-    
-    
 #     return render(request,('home.html'))
-
-
 
 
 def home (request):  
 
-    #FUNCIONA NO TOPAR
+    # Ejemplo 1  FUNCIONA NO TOPAR
     # figura, variablederango = plt.subplots()
 
     # variablederango.set_xlim(1, 20)
     # variablederango.set_ylim(1, 20)
     # variablederango.set_box_aspect(1)
 
-    #FUNCIONA NO TOPAR
+    # Ejemplo 2FUNCIONA NO TOPAR
     # figura, (linea,rango)= plt.subplots(ncols=2, sharey=True)
     # linea.plot([1,0],[0,10])
     # rango.plot([0,10],[0,10])
     #linea.set_box_aspect(1)
     #rango.set_box_aspect(1)
 
-    #NO TOPAR
+    # Ejemplo 3 NO TOPAR
     # t = np.linspace(0.0, 1.0, 100)
     # s = np.tan(4 * np.pi * t) + 2
     # figura, ax = plt.subplots(figsize=(6, 4), tight_layout=True)
@@ -55,15 +39,14 @@ def home (request):
     # ax.set_xlabel(r'Rango en X', fontsize=8)
     # ax.set_ylabel('Rango en Y', fontsize=8)
 
-    #NO TOPAR
+    # Ejemplo 4 NO TOPAR
     # x = np.linspace(-10, 10, 1000)
     # y = x**2 + 2*x + 2 
     # figura, ax = plt.subplots()
     # ax.plot(x, y)
 
-    m = any
     x = np.linspace(-5,5,4000)
-    y = np.m(x)
+    y = np.sin(x)
 
     # figura
     figura = plt.figure()
